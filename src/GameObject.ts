3f2ef3cd80aa393abf3ffdeafe231aeb6fbb1237
dawidwyrkowski <dawidwyrkowski@gmail.com>
2023-04-06 15:45:52 +0200

    chore: comment with idea

--- src/GameObject.ts
+++ src/GameObject.ts
@@ -6,4 +6,12 @@ export class GameObject {
 
     // how to render it?
     // should probably return some config on end and then merge it with other configs
+    // I should have here stable object or unstable objects information to check if I can somehow interact with object
+
+    // dependecy loop idea:
+    // 1. create information data about stable or unstable object and pass it to gameObjects that can move
+    // (with Handlers or maybe just class Character withc will be loaded on other endpoint)
+    // 2. create calculation on character and create some data information from it
+    // 3. pass it to unstable object so we can interact with them (eg fire torch, little grass movement etc)
+    // 4. render all things from gameObjects or/and character object
 }
